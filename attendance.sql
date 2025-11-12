6aec176178ad17170450601fbece74ba75319e4d
viswa <viswadiago28@gmail.com>
2025-11-12 11:54:25 +0530

    final-commit

---
+++ attendance.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2025 at 07:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `attendance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `checkin_time` time DEFAULT NULL,
+  `checkout_time` time DEFAULT NULL,
+  `total_hours` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `user_id`, `date`, `checkin_time`, `checkout_time`, `total_hours`) VALUES
+(13, 4, '2025-11-12', '03:05:57', '03:07:31', '00:01:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','employee') DEFAULT 'employee',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`) VALUES
+(1, 'admin', 'admin@gmail.com', '121212', 'admin', '2025-11-11 15:21:08'),
+(2, 'viswa', 'viswa@gmail.com', '121212', 'employee', '2025-11-11 15:21:49'),
+(4, 'test', 'test@gmail.com', '121212', 'employee', '2025-11-11 15:23:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.txt
@@ -0,0 +1,61 @@
+# 🧑‍💼 Employee Task Management System
+
+A simple **web-based task management system** built with **Core PHP** and **MySQL** that helps administrators assign, track, and manage employee tasks efficiently.
+
+---------------------------------------------------------------------------------------------
+
+## 🚀 Features
+
+- 👨‍💼 Admin login system  
+- 🧑‍🔧 Employee login system  
+- 📋 Admin can view the employees working hrs 
+- ✅ Employees can checkin and checkout 
+- 🔒 Secure authentication and session handling  
+- 🧭 Clean and simple dashboard for admin  
+
+--------------------------------------------------------------------------------------------
+
+## 🧱 Tech Stack
+
+| Category    | Technology Used |
+|--------------|----------------|
+| Frontend     | HTML, CSS, JavaScript |
+| Backend      | Core PHP |
+| Database     | MySQL |
+| Server       | XAMPP  |
+
+--------------------------------------------------------------------------------------------
+
+1. **Clone the repository**
+   ```bash
+   git clone https://github.com/Viswa-28/employee-task.git
+Move the folder to your web server directory (e.g., htdocs for XAMPP).
+
+Import the database
+
+Open phpMyAdmin
+
+Create a new database (attendance)
+
+Import the SQL file provided in the repo
+
+Update your database credentials inside config/config.php
+
+Run the project
+
+
+
+-------------------------------------------------------------------------------------------
+
+Login credentials
+
+user-
+email:viswa@gmail.com
+password:121212
+
+admin
+email:admin@gmail.com
+password:121212
+
+
+
